d300fcb7e661efb015a98b6160a25ca139d33b89
Juliien <julien1105@outlook.fr>
2021-08-18 00:46:23 +0200

    feat: refactoring hello()

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      project: 'fordem-api',
+      version: '1.0.0',
+      members: ['Julien DA CORTE']
+    }
   }
 }
