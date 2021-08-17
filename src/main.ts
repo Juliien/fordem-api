eaaa44369ea67fd911f1c5ff6cfdd25f5f1b5434
Juliien <julien1105@outlook.fr>
2021-08-18 00:39:02 +0200

    feat: deploy to heroku

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 require('dotenv').config();
-const { PORT, API_PREFIX } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix(API_PREFIX);
-  await app.listen(PORT || 3000);
+  app.setGlobalPrefix(process.env.API_PREFIX);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap().then();
